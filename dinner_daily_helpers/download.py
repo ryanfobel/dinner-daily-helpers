cb12a1d22ad03c3a93d1b89bcfd4b7fa064dcf1a
Christian Fobel <christian@fobel.net>
2020-02-22 13:51:14 -0500

    feat(download): simulate user/passwd login
    Prior to this commit, menu and shopping list downloads used user browser
    cookies for authentication. This sometimes failed due to, e.g., expired
    cookies, where it was necessary to manually log in to the Dinner Daily
    website again to refresh the cookie.
    
    In this commit, modify download code to authenticate directly with the
    Dinner Daily website using a username and password. This ensures that
    a new cookie is obtained each time. It also eliminates the need for
    access to the user's browser cookies.
    
    Username and password may be set using the following environment
    variables, respectively:
    
     - `DINNER_DAILY_USERNAME`
     - `DINNER_DAILY_PASSWORD`
    
    BREAKING CHANGE: deprecate use of user browser cookies for downloads;
    instead require username/password for authentication.

--- dinner_daily_helpers/download.py
+++ dinner_daily_helpers/download.py
@@ -4,31 +4,68 @@ import os
 import re
 
 import dateparser
+import requests
 
-from .memoized import cookie_request, DEFAULT_STORE
 from .menu import extract_menu
 
+DEFAULT_STORE = os.environ.get('DINNER_DAILY_STORE', 'Any Store')
 
-def download(week_, output_dir, store=DEFAULT_STORE):
+
+def login(username, password):
+    '''
+    Log in to the Dinner Daily website.
+
+    Returns
+    -------
+    requests.Session
+        Use session methods (e.g., ``get()``, ``post()``) to use
+        authenticated access.
+    '''
+    session = requests.Session()
+    url = 'https://thedinnerdaily.com/cms/wp-login.php'
+
+    data = {'log': username, 'pwd': password,
+            'wp-submit': 'Log In',
+            'redirect_to': 'https://thedinnerdaily.com/cms/wp-admin/',
+            'testcookie': 1}
+    headers = {'referer': 'https://thedinnerdaily.com/'}
+    response = session.get(url)
+    response = session.post(url, data=data, headers=headers)
+    assert(response.status_code == 200)
+    return session
+
+
+def download(week_, output_dir, session=None, username=None, password=None,
+             store=DEFAULT_STORE):
     '''
     .. versionchanged:: X.X.X
         Parse start date from new ``<month> <start> to <end>`` format (e.g.,
         ``Oct 28 to 03``).
+    .. versionchanged:: X.X.X
+        Add ``username`` and ``password`` kwargs. Use these to authenticate
+        session for download.
     '''
     if week_ not in ('current', 'previous'):
         raise ValueError('`week` must be either `current` or `previous`.')
 
     base_url = 'https://db.thedinnerdaily.com/menus/'
 
+    if session is None:
+        if any((username is None, password is None)):
+            raise ValueError('Either `session` or `username` _and_ `password` '
+                             'must be specified.')
+        else:
+            session = login(username, password)
+
     # Download weekly menu using browser cookies.
-    menu_response = cookie_request(base_url + 'print/%s/%s' % (store, week_))
+    menu_response = session.get(base_url + 'print/%s/%s' % (store, week_))
     # Scrape date from menu HTML to use in file name.
     menu = extract_menu(menu_response.text)
     menu['date'] = re.sub(r' to .*', '', menu['date'])
     menu_date = dateparser.parse(menu['date'])
     # Download weekly shopping list using browser cookies.
-    list_response = cookie_request(base_url + 'print-shopping-list/%s/%s' %
-                                   (store, week_))
+    list_response = session.get(base_url + 'print-shopping-list/%s/%s' %
+                                (store, week_))
 
     cwd = os.getcwd()
     try:
@@ -38,12 +75,12 @@ def download(week_, output_dir, store=DEFAULT_STORE):
 
         out_name = out_name_fmt % 'weekly-menu'
         with open(out_name, 'w') as output:
-            output.write(menu_response.text.encode('utf8'))
+            output.write(menu_response.text)
         logging.info('Wrote weekly menu to: `%s`' % out_name)
 
         out_name = out_name_fmt % 'shopping-list'
         with open(out_name, 'w') as output:
-            output.write(list_response.text.encode('utf8'))
+            output.write(list_response.text)
         logging.info('Wrote weekly menu to: `%s`' % out_name)
     finally:
         os.chdir(cwd)
@@ -54,6 +91,14 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('week', choices=('current', 'previous'))
+    parser.add_argument('--username',
+                        default=os.environ.get('DINNER_DAILY_USERNAME'),
+                        help='Dinner Daily username (default: '
+                        '`DINNER_DAILY_USERNAME` environment variable).')
+    parser.add_argument('--password',
+                        default=os.environ.get('DINNER_DAILY_PASSWORD'),
+                        help='Dinner Daily password (default: '
+                        '`DINNER_DAILY_PASSWORD` environment variable).')
     parser.add_argument('--store', default=DEFAULT_STORE, help='Store '
                         '(default: %(default)s)')
     parser.add_argument('output_dir', help='Output directory.')
@@ -64,4 +109,5 @@ def parse_args():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     args = parse_args()
-    download(args.week, args.output_dir, store=args.store)
+    download(args.week, args.output_dir, username=args.username,
+             password=args.password, store=args.store)
