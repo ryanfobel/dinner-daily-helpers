88dafd0f917e601e425dccf11622eb2a35ed60c7
Christian Fobel <christian@fobel.net>
2020-02-23 22:22:18 -0500

    fixup

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -4,12 +4,12 @@ Set-ExecutionPolicy RemoteSigned
 # Set version number based on git tag
 $x = git describe --tags
 if (!$?) {
-    $prevTag = \"v0.0-0\"
+    $prevTag = "v0.0-0"
 }
-else { $prevTag = $x.Split(\"-\")[0] + \"-\" + $x.Split(\"-\")[1] }
+else { $prevTag = $x.Split("-")[0] + "-" + $x.Split("-")[1] }
 
-$buildTag = $prevTag + \"+\" + $(Get-Date -Format FileDateTime)
-Write-Host \"Build Tag: $buildTag\"
+$buildTag = $prevTag + "+" + $(Get-Date -Format FileDateTime)
+Write-Host "Build Tag: $buildTag"
 Update-AppveyorBuild -Version $buildTag
 
 conda activate $env:APPVEYOR_PROJECT_NAME
@@ -17,17 +17,17 @@ conda activate $env:APPVEYOR_PROJECT_NAME
 python -m dinner_daily_helpers.download previous .
 foreach ($menu in $(dir ????-??-??-weekly-menu-Any?Store.html)) { 
     echo $menu;
-    if (-not (Test-Path \"$($menu.BaseName).json\")) {
-        echo \"Converting $menu to JSON\";
-        python -m dinner_daily_helpers --json $menu \"$($menu.BaseName).json\";
+    if (-not (Test-Path "$($menu.BaseName).json")) {
+        echo "Converting $menu to JSON";
+        python -m dinner_daily_helpers --json $menu "$($menu.BaseName).json";
     }
-    if (-not (Test-Path \"$($menu.BaseName).md\")) {
-        echo \"Converting $menu to Markdown\";
-        python -m dinner_daily_helpers --markdown $menu \"$($menu.BaseName).md\";
+    if (-not (Test-Path "$($menu.BaseName).md")) {
+        echo "Converting $menu to Markdown";
+        python -m dinner_daily_helpers --markdown $menu "$($menu.BaseName).md";
     }
-    if (-not (Test-Path \"$($menu.BaseName).reformatted.html\")) {
-        echo \"Converting $menu to reformatted HTML\";
-        python -m dinner_daily_helpers $menu \"$($menu.BaseName).reformatted.html\";
+    if (-not (Test-Path "$($menu.BaseName).reformatted.html")) {
+        echo "Converting $menu to reformatted HTML";
+        python -m dinner_daily_helpers $menu "$($menu.BaseName).reformatted.html";
     }
 }
 
@@ -35,4 +35,4 @@ md artifacts
 mv *.json artifacts
 mv *.html artifacts
 mv *.md artifacts
-if (-Not $?) { throw \"No markdown files found.\" }
+if (-Not $?) { throw "No markdown files found." }
